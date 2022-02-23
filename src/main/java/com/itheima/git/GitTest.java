7575b2b09cbb0d4effd60d4695625b9220fab100
zsc <2267945782@qq.com>
2022-02-23 15:35:06 +0800

    second commit

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.printf("hello git");
+        System.out.printf("hello github");
     }
 }
